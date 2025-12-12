bc47724caf6392050292f62269dcb195d238de84
Jairo Oliveira Santos <jairobr1986@gmail.com>
2025-12-11 22:48:40 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img");
+
+  if (html.classList.contains('light')) {
+    img.setAttribute('src', './assets/github-jairo.jpg')
+  } else {
+    img.setAttribute('src', './assets/perfil.jpg')
+
+  }
+
+
+}
